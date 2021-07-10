fe47a98bc8b72a0885d9eadeced9883860634eb8
Kai Krings <kai.krings@posteo.de>
2021-07-10 13:01:06 +0200

    Fix grammar.

--- src/components/cipher-app-bar/CipherAppBarInfoMenu.tsx
+++ src/components/cipher-app-bar/CipherAppBarInfoMenu.tsx
@@ -16,7 +16,7 @@ export function CipherAppBarInfoMenu(props: CipherAppBarInfoMenuProps) {
       <Typography variant="h6">Datenschutzerklärung</Typography>
       <Typography variant="body2" paragraph={true}>
         Diese Applikation speichert keinerlei persönliche Daten. Es werden weder Cookies
-        und noch Tools wie Google Analytics verwendet.
+        noch Tools wie Google Analytics verwendet.
       </Typography>
     </Paper>
   );
