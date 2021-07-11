00e98327d97dfac34fb3b42d69bb28a6a4c341a5
Kai Krings <kai.krings@posteo.de>
2021-07-11 14:30:15 +0200

    Fix wrong test description.

--- src/components/cipher-app-bar/CipherAppBar.test.tsx
+++ src/components/cipher-app-bar/CipherAppBar.test.tsx
@@ -3,7 +3,7 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { CipherAppBar } from './CipherAppBar';
 
-describe('CipherAppBarInfoMenu', () => {
+describe('CipherAppBar', () => {
   beforeEach(() => {
     render(<CipherAppBar />);
   });
