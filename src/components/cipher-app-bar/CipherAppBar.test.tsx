2095beecd6c036506a189646ae50a02fb6a8fca5
Kai Krings <kai.krings@posteo.de>
2021-07-11 11:02:38 +0200

    Add unit tests for CipherAppBar component.

---
+++ src/components/cipher-app-bar/CipherAppBar.test.tsx
@@ -0,0 +1,50 @@
+import * as React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { CipherAppBar } from './CipherAppBar';
+
+describe('CipherAppBarInfoMenu', () => {
+  beforeEach(() => {
+    render(<CipherAppBar />);
+  });
+
+  it('info popup should be closed', () => {
+    expect(screen.queryByRole('tooltip')).toBeNull()
+  });
+
+  describe('click on info button', () => {
+    beforeEach(() => {
+      userEvent.click(screen.getByLabelText('Datenschutzerklärung'));
+    });
+
+    it('info popup should have been opened', () => {
+      expect(screen.queryByRole('tooltip')).toBeInTheDocument()
+    });
+  });
+
+  describe('click on info button when info popup is open', () => {
+    beforeEach(() => {
+      const button = screen.getByLabelText('Datenschutzerklärung')
+      userEvent.click(button);
+      userEvent.click(button);
+    });
+
+    it('info popup should have been closed', () => {
+      expect(screen.queryByRole('tooltip')).toBeNull()
+    });
+  });
+
+  describe('click on close button', () => {
+    beforeEach(() => {
+      userEvent.click(screen.getByLabelText('Datenschutzerklärung'));
+    });
+
+    beforeEach(() => {
+      userEvent.click(screen.getByRole('close'));
+    });
+
+    it('info popup should have been closed', () => {
+      expect(screen.queryByRole('tooltip')).toBeNull()
+    });
+  });
+});
