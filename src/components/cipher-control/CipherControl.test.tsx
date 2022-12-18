80bbd619fb376c4b01d422c5a54cf7812b78ffab
Kai Krings <kai.krings@posteo.de>
2022-12-18 13:04:17 +0000

    Fix CipherControl.test.tsx.

--- src/components/cipher-control/CipherControl.test.tsx
+++ src/components/cipher-control/CipherControl.test.tsx
@@ -3,49 +3,44 @@ import userEvent from '@testing-library/user-event';
 import { render, screen } from '@testing-library/react';
 import { CipherControl } from './CipherControl';
 
-describe('CipherControl', () => {
-  it('onClockwiseRotation should have been called', () => {
-    const onClockwiseRotation = jest.fn();
+test('rotate clockwise', async () => {
+  const user = userEvent.setup();
+  const onClockwiseRotation = jest.fn();
+  render(
+    <CipherControl
+      onClockwiseRotation={onClockwiseRotation}
+      onCounterClockwiseRotation={() => {}}
+      onReset={() => {}}
+    />,
+  );
+  await user.click(screen.getByLabelText('Im Uhrzeigersinn'));
+  expect(onClockwiseRotation).toBeCalledTimes(1);
+});
 
-    render(
-      <CipherControl
-        onClockwiseRotation={onClockwiseRotation}
-        onCounterClockwiseRotation={() => {}}
-        onReset={() => {}}
-      />,
-    );
+it('rotate counter-clockwise', async () => {
+  const user = userEvent.setup();
+  const onCounterClockwiseRotation = jest.fn();
+  render(
+    <CipherControl
+      onClockwiseRotation={() => {}}
+      onCounterClockwiseRotation={onCounterClockwiseRotation}
+      onReset={() => {}}
+    />,
+  );
+  await user.click(screen.getByLabelText('Gegen den Uhrzeigersinn'));
+  expect(onCounterClockwiseRotation).toBeCalledTimes(1);
+});
 
-    userEvent.click(screen.getByLabelText('Im Uhrzeigersinn'));
-    expect(onClockwiseRotation).toBeCalledTimes(1);
-  });
-
-  it('onCounterClockwiseRotation should have been called', () => {
-    const onCounterClockwiseRotation = jest.fn();
-
-    render(
-      <CipherControl
-        onClockwiseRotation={() => {}}
-        onCounterClockwiseRotation={onCounterClockwiseRotation}
-        onReset={() => {}}
-      />,
-    );
-
-    userEvent.click(screen.getByLabelText('Gegen den Uhrzeigersinn'));
-    expect(onCounterClockwiseRotation).toBeCalledTimes(1);
-  });
-
-  it('onReset should have been called', () => {
-    const onReset = jest.fn();
-
-    render(
-      <CipherControl
-        onClockwiseRotation={() => {}}
-        onCounterClockwiseRotation={() => {}}
-        onReset={onReset}
-      />,
-    );
-
-    userEvent.click(screen.getByLabelText('Auf Anfang'));
-    expect(onReset).toBeCalledTimes(1);
-  });
+test('reset', async () => {
+  const user = userEvent.setup();
+  const onReset = jest.fn();
+  render(
+    <CipherControl
+      onClockwiseRotation={() => {}}
+      onCounterClockwiseRotation={() => {}}
+      onReset={onReset}
+    />,
+  );
+  await user.click(screen.getByLabelText('Auf Anfang'));
+  expect(onReset).toBeCalledTimes(1);
 });
