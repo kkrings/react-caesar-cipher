60ae375088d85bdf35698a50ef6e75555dd5502b
Kai Krings <kai.krings@posteo.de>
2022-12-18 17:32:29 +0000

    Reduce indentation and rename test.

--- src/components/cipher-app-bar/cipher-app-bar-info-menu/CipherAppBarInfoMenu.test.tsx
+++ src/components/cipher-app-bar/cipher-app-bar-info-menu/CipherAppBarInfoMenu.test.tsx
@@ -3,12 +3,10 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { CipherAppBarInfoMenu } from './CipherAppBarInfoMenu';
 
-test('handleOnCloseButtonClick should have been called', async () => {
+test('click close button', async () => {
   const user = userEvent.setup();
-  const handleOnCloseButtonClick = jest.fn();
-  render(
-    <CipherAppBarInfoMenu onCloseButtonClick={handleOnCloseButtonClick} />,
-  );
+  const handleClick = jest.fn();
+  render(<CipherAppBarInfoMenu onCloseButtonClick={handleClick} />);
   await user.click(screen.getByRole('close'));
-  expect(handleOnCloseButtonClick).toHaveBeenCalledTimes(1);
+  expect(handleClick).toHaveBeenCalledTimes(1);
 });
