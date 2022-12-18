7656467a9a1493d33b495984139cfd7aca348ecb
Kai Krings <kai.krings@posteo.de>
2022-12-18 17:22:43 +0000

    Fix App.test.tsx.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,129 +4,120 @@ import { render, screen } from '@testing-library/react';
 import { App } from './App';
 import { numLatinLetters } from './services/CipherService';
 
-describe('App', () => {
-  const message = 'ABC';
-  const shift = 1;
-  const angleDeg = 360 / numLatinLetters;
+const message = 'ABC';
+const angleDeg = 360 / numLatinLetters;
+const shift = 1;
 
-  beforeEach(() => render(<App />));
-  beforeEach(() =>
-    userEvent.type(screen.getByLabelText('Geheime Nachricht'), message),
-  );
+async function rotateCipherDiskCounterClockwise(): Promise<void> {
+  const user = userEvent.setup();
+  render(<App />);
+  await user.type(screen.getByLabelText('Geheime Nachricht'), message);
+  await user.click(screen.getByLabelText('Gegen den Uhrzeigersinn'));
+}
 
-  describe('click counter-clockwise rotation button once', () => {
-    const encryptedMessage = 'BCD';
-    const expectedShift = shift;
+async function rotateCipherDiskClockwise(): Promise<void> {
+  const user = userEvent.setup();
+  render(<App />);
+  await user.type(screen.getByLabelText('Geheime Nachricht'), message);
+  await user.click(screen.getByLabelText('Im Uhrzeigersinn'));
+}
 
-    beforeEach(() => {
-      userEvent.click(screen.getByLabelText('Gegen den Uhrzeigersinn'));
-    });
+async function resetCipherDisk(): Promise<void> {
+  const user = userEvent.setup();
+  render(<App />);
+  await user.type(screen.getByLabelText('Geheime Nachricht'), message);
+  await user.click(screen.getByLabelText('Im Uhrzeigersinn'));
+  await user.click(screen.getByLabelText('Auf Anfang'));
+}
 
-    it('cipher disk should have been rotated counter-clockwise', () => {
-      expect(screen.getByRole('img')).toHaveStyle({
-        transform: `rotate(-${angleDeg}deg)`,
-      });
-    });
+async function rotateCipherDiskBack(): Promise<void> {
+  const user = userEvent.setup();
+  render(<App />);
+  await user.type(screen.getByLabelText('Geheime Nachricht'), message);
+  await user.click(screen.getByLabelText('Gegen den Uhrzeigersinn'));
+  await user.click(screen.getByLabelText('Im Uhrzeigersinn'));
+}
 
-    it(`shift of ${expectedShift} should have been rendered`, () => {
-      expect(screen.getByTestId('shift').textContent).toMatch(
-        `Anzahl Verschiebungen: ${expectedShift}`,
-      );
-    });
+test('rotate cipher disk counter-clockwise: disk rotation', async () => {
+  await rotateCipherDiskCounterClockwise();
+  const element = screen.getByRole('img');
+  const style = { transform: `rotate(-${angleDeg}deg)` };
+  expect(element).toHaveStyle(style);
+});
 
-    it(`${message} should have been encrypted to ${encryptedMessage}`, () => {
-      expect(screen.getByLabelText('Verschlüsselte Nachricht')).toHaveValue(
-        encryptedMessage,
-      );
-    });
-  });
+test('rotate cipher disk counter-clockwise: number of shifts', async () => {
+  await rotateCipherDiskCounterClockwise();
+  const element = screen.getByTestId('shift');
+  const textContent = `Anzahl Verschiebungen: ${shift}`;
+  expect(element).toHaveTextContent(textContent);
+});
 
-  describe('click clockwise rotation button once', () => {
-    const encryptedMessage = 'ZAB';
-    const expectedShift = numLatinLetters - shift;
+test('rotate cipher disk counter-clockwise: encrypted message', async () => {
+  await rotateCipherDiskCounterClockwise();
+  const element = screen.getByLabelText('Verschlüsselte Nachricht');
+  const value = 'BCD';
+  expect(element).toHaveValue(value);
+});
 
-    beforeEach(() => {
-      userEvent.click(screen.getByLabelText('Im Uhrzeigersinn'));
-    });
+test('rotate cipher disk clockwise: disk rotation', async () => {
+  await rotateCipherDiskClockwise();
+  const element = screen.getByRole('img');
+  const style = { transform: `rotate(${angleDeg}deg)` };
+  expect(element).toHaveStyle(style);
+});
 
-    it('cipher disk should have been rotated clockwise', () => {
-      expect(screen.getByRole('img')).toHaveStyle({
-        transform: `rotate(${angleDeg}deg)`,
-      });
-    });
+test('rotate cipher disk clockwise: number of shifts', async () => {
+  await rotateCipherDiskClockwise();
+  const element = screen.getByTestId('shift');
+  const textContent = `Anzahl Verschiebungen: ${numLatinLetters - shift}`;
+  expect(element).toHaveTextContent(textContent);
+});
 
-    it(`shift of ${expectedShift} should have been rendered`, () => {
-      expect(screen.getByTestId('shift').textContent).toMatch(
-        `Anzahl Verschiebungen: ${expectedShift}`,
-      );
-    });
+test('rotate cipher disk clockwise: encrypted message', async () => {
+  await rotateCipherDiskClockwise();
+  const element = screen.getByLabelText('Verschlüsselte Nachricht');
+  const value = 'ZAB';
+  expect(element).toHaveValue(value);
+});
 
-    it(`${message} should have been encrypted to ${encryptedMessage}`, () => {
-      expect(screen.getByLabelText('Verschlüsselte Nachricht')).toHaveValue(
-        encryptedMessage,
-      );
-    });
-  });
+test('reset cipher disk: disk rotation', async () => {
+  await resetCipherDisk();
+  const element = screen.getByRole('img');
+  const style = { transform: `rotate(0deg)` };
+  expect(element).toHaveStyle(style);
+});
 
-  describe('click reset button once', () => {
-    const encryptedMessage = message;
-    const expectedShift = 0;
+test('reset cipher disk: number of shifts', async () => {
+  await resetCipherDisk();
+  const element = screen.getByTestId('shift');
+  const textContent = 'Anzahl Verschiebungen: 0';
+  expect(element).toHaveTextContent(textContent);
+});
 
-    beforeEach(() => {
-      userEvent.click(screen.getByLabelText('Im Uhrzeigersinn'));
-    });
+test('reset cipher disk: encrypted message', async () => {
+  await resetCipherDisk();
+  const element = screen.getByLabelText('Verschlüsselte Nachricht');
+  const value = message;
+  expect(element).toHaveValue(value);
+});
 
-    beforeEach(() => {
-      userEvent.click(screen.getByLabelText('Auf Anfang'));
-    });
+test('rotate cipher disk back: disk rotation', async () => {
+  await rotateCipherDiskBack();
+  const element = screen.getByRole('img');
+  const style = { transform: `rotate(0deg)` };
+  expect(element).toHaveStyle(style);
+});
 
-    it('cipher disk should have been rotated back to zero', () => {
-      expect(screen.getByRole('img')).toHaveStyle({
-        transform: 'rotate(0deg)',
-      });
-    });
+test('rotate cipher disk back: number of shifts', async () => {
+  await rotateCipherDiskBack();
+  const element = screen.getByTestId('shift');
+  const textContent = 'Anzahl Verschiebungen: 0';
+  expect(element).toHaveTextContent(textContent);
+});
 
-    it(`shift of ${expectedShift} should have been rendered`, () => {
-      expect(screen.getByTestId('shift').textContent).toMatch(
-        `Anzahl Verschiebungen: ${expectedShift}`,
-      );
-    });
-
-    it(`${message} should have been encrypted to ${encryptedMessage}`, () => {
-      expect(screen.getByLabelText('Verschlüsselte Nachricht')).toHaveValue(
-        encryptedMessage,
-      );
-    });
-  });
-
-  describe('rotate counter-clockwise rotated cipher disk clockwise', () => {
-    const encryptedMessage = message;
-    const expectedShift = 0;
-
-    beforeEach(() => {
-      userEvent.click(screen.getByLabelText('Gegen den Uhrzeigersinn'));
-    });
-
-    beforeEach(() => {
-      userEvent.click(screen.getByLabelText('Im Uhrzeigersinn'));
-    });
-
-    it('cipher disk should have been rotated back to zero', () => {
-      expect(screen.getByRole('img')).toHaveStyle({
-        transform: 'rotate(0deg)',
-      });
-    });
-
-    it(`shift of ${expectedShift} should have been rendered`, () => {
-      expect(screen.getByTestId('shift').textContent).toMatch(
-        `Anzahl Verschiebungen: ${expectedShift}`,
-      );
-    });
-
-    it(`${message} should have been encrypted to ${encryptedMessage}`, () => {
-      expect(screen.getByLabelText('Verschlüsselte Nachricht')).toHaveValue(
-        encryptedMessage,
-      );
-    });
-  });
+test('rotate cipher disk back: encrypted message', async () => {
+  await rotateCipherDiskBack();
+  const element = screen.getByLabelText('Verschlüsselte Nachricht');
+  const value = message;
+  expect(element).toHaveValue(value);
 });
