c1f61855c7f292ec0acdd8fcc908f56b604f175c
Kai Krings <kai.krings@posteo.de>
2022-12-18 12:58:37 +0000

    Fix CipherDisks.test.tsx.

--- src/components/cipher-disks/CipherDisks.test.tsx
+++ src/components/cipher-disks/CipherDisks.test.tsx
@@ -2,16 +2,9 @@ import * as React from 'react';
 import { render, screen } from '@testing-library/react';
 import { CipherDisks } from './CipherDisks';
 
-describe('CipherDisks', () => {
+test('rotate image', () => {
   const angleDeg = 45;
-
-  beforeEach(() => {
-    render(<CipherDisks angleDeg={angleDeg} />);
-  });
-
-  it('style should contain transform property', () => {
-    expect(screen.getByRole('img')).toHaveStyle({
-      transform: `rotate(${angleDeg}deg)`,
-    });
-  });
+  render(<CipherDisks angleDeg={angleDeg} />);
+  const transform = `rotate(${angleDeg}deg)`;
+  expect(screen.getByRole('img')).toHaveStyle({ transform });
 });
