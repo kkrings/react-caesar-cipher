a07faba94ad0e87cd1cbff11d6019c5b11b7e52b
Kai Krings <kai.krings@posteo.de>
2023-05-19 17:26:49 +0000

    Setting correct base URL for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
+  base: '/react-caesar-cipher/',
   plugins: [react()],
   test: {
     environment: 'jsdom',
